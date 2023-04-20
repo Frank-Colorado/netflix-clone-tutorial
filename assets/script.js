0022b387ca85733197c169d7e66cfb1df3724553
Frank-Colorado <chawnkiee@gmail.com>
2023-04-20 19:41:38 -0400

    created fetchMovies function

--- assets/script.js
+++ assets/script.js
@@ -2,3 +2,10 @@
 const originalsDiv = document.querySelector(".netflixOriginals");
 const trendingDiv = document.getElementById("trending");
 const topRatedDiv = document.getElementById("top_rated");
+
+// function that fetches API
+const fetchMovies = (url) => {
+  fetch(url)
+    .then((response) => response.json())
+    .then((movies) => console.log(movies));
+};
