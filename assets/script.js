2fb4ec8bb0b8ec69127374a3ec74c61509c78e8a
Frank-Colorado <chawnkiee@gmail.com>
2023-04-20 19:45:26 -0400

    added a function called 'getOriginals'

--- assets/script.js
+++ assets/script.js
@@ -3,9 +3,18 @@ const originalsDiv = document.querySelector(".netflixOriginals");
 const trendingDiv = document.getElementById("trending");
 const topRatedDiv = document.getElementById("top_rated");
 
-// function that fetches API
+// function that fetches data from movie API
 const fetchMovies = (url) => {
   fetch(url)
     .then((response) => response.json())
     .then((movies) => console.log(movies));
 };
+
+// function that fetches Netflix Originals
+const getOriginals = () => {
+  let url =
+    "https://api.themoviedb.org/3/discover/tv?api_key=19f84e11932abbc79e6d83f82d6d1045&with_networks=213";
+  fetchMovies(url);
+};
+
+getOriginals();
