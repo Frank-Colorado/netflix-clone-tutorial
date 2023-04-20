f2a4d1582d85864ecde9f65a645ccea95148de50
Frank-Colorado <chawnkiee@gmail.com>
2023-04-20 17:07:37 -0400

    added DOM element variables to js file

---
+++ assets/script.js
@@ -0,0 +1,4 @@
+// DOM element variables
+const originalsDiv = document.querySelector(".netflixOriginals");
+const trendingDiv = document.getElementById("trending");
+const topRatedDiv = document.getElementById("top_rated");
